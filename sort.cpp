ef7f08d1e62300550d3ff47535c6cfe134269c4c
sumin <keepthrword22@snu.ac.kr>
2023-05-11 22:18:18 +0900

    Init repository

---
+++ sort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello world!"<<endl;
+    return 0;
+}
+
+// insertion sort
+
+// merge sort
+
+// quick sort
+
+// generate array in ascending order
+
+// generate array in descending order
+
+// generate array in random order
